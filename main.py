e73756c42133b5fcfc48a84339745fa13399f80d
aidan <aidanbelch@gmail.com>
2023-03-14 14:48:55 +1300

    Commit Message

--- main.py
+++ main.py
@@ -1,2 +1,64 @@
-import pygame
-print("Retaining wall.")
+import tkinter as tk
+
+#This does somthing
+application = tk.Tk()
+
+#Make the name and size
+application.title("SEE SAW SIE FER")
+application.geometry("1920x1080")
+
+def Gaming():
+    application.mainloop()
+
+Gaming()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
